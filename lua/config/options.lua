9ef0f03e89baeabe8f78e9155f0f7fca4122af9b
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2024-05-17 23:03:10 -0400

    feat: add lazygit

---
+++ lua/config/options.lua
@@ -0,0 +1,5 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- Genera en la barra información importante como por ejemplo el archivo actual
+vim.opt.winbar = "%=%m %f %r%h%w %y %c,%l/%L %P"
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", opts = { flavour = "macchiato" } },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
