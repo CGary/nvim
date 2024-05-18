33ee98c6f0797325019e18e16d5a7bd8813a7dfb
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2024-05-18 12:36:50 -0400

    feat: add suda.vim

---
+++ lua/plugins/suda.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "lambdalisue/suda.vim",
+    config = function()
+      -- Configuración opcional
+      vim.g.suda_smart_edit = 1
+      vim.g.suda_prompt = "Password: "
+    end,
+  },
+}
